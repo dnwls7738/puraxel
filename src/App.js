5db4db669091d91b7e8c2587aa9a3f6042238781
wjcheon <wjcheon@LMNAS.COM>
2022-07-27 14:47:34 +0900

    20220727

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Fx5000 from "./components/pages/Fx5000";
 import Hair from "./components/pages/Hair";
@@ -6,16 +6,31 @@ import Main from "./components/pages/Main";
 import Me from "./components/pages/Me";
 import PuraTech from "./components/pages/PuraTech";
 
-const setScreenSize = () => {
-  let vh = window.innerHeight * 0.01;
+// const setScreenSize = () => {
+//   let vh = window.innerHeight * 0.01;
 
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-  window.addEventListener("resize", () => setScreenSize());
-};
-setScreenSize();
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
+// };
+// setScreenSize();
+// window.addEventListener("resize", () => setScreenSize());
 
 function App() {
+  let vh = 0;
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
+  const setVh = () => {
+    document.documentElement.style.setProperty(
+      "--vh",
+      `${window.innerHeight}px`
+    );
+  };
+  window.addEventListener("resize", setVh);
+  setVh();
+
   return (
     <Router>
       <Route path="/" exact component={Main} />
