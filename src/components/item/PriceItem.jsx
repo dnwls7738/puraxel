d5ef70b991a2a08c8f25a0923ac53cb11f88788a
dnwls7738 <cjsdnwls7738@gmail.com>
2022-08-04 16:34:25 +0900

    20220804

--- src/components/item/PriceItem.jsx
+++ src/components/item/PriceItem.jsx
@@ -11,34 +11,48 @@ function PriceItem() {
         </div>
 
         <div className="contPrice">
-          <div className="price1">
-            <div className="name">
-              <p className="text">퓨라셀 ME</p>
-              <p className="num">1,890,000원</p>
+          <Link
+            to={{
+              pathname: "https://lmdt.cafe24.com",
+            }}
+            target="_blank"
+          >
+            <div className="price1">
+              <div className="name">
+                <p className="text">퓨라셀 ME</p>
+                <p className="num">1,890,000원</p>
+              </div>
+              <div className="sub">
+                <p className="subText">
+                  12개월 <em>월 157,500원</em>
+                </p>
+              </div>
             </div>
-            <div className="sub">
-              <p className="subText">
-                12개월 <em>월 157,500원</em>
-              </p>
+          </Link>
+          <Link
+            to={{
+              pathname: "https://lmdt.cafe24.com",
+            }}
+            target="_blank"
+          >
+            <div className="price2">
+              <div className="name">
+                <p className="text">
+                  퓨라셀 ME+ <br />
+                  퓨라더마앰플
+                </p>
+                <p className="num">
+                  <em>2,433,000원 -&gt;</em> 1,890,000원
+                </p>
+              </div>
+              <div className="sub">
+                <p className="sale">23% off</p>
+                <p className="subText">
+                  12개월 <em>월 157,500원</em>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="price2">
-            <div className="name">
-              <p className="text">
-                퓨라셀 ME+ <br />
-                퓨라더마앰플
-              </p>
-              <p className="num">
-                <em>2,433,000원 -&gt;</em> 1,890,000원
-              </p>
-            </div>
-            <div className="sub">
-              <p className="sale">23% off</p>
-              <p className="subText">
-                12개월 <em>월 157,500원</em>
-              </p>
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
     </>
