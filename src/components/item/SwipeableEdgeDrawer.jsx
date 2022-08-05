09cd19f1901040323b7b8fefb11b22adc7704cc7
dnwls7738 <cjsdnwls7738@gmail.com>
2022-08-05 17:58:13 +0900

    20220805

--- src/components/item/SwipeableEdgeDrawer.jsx
+++ src/components/item/SwipeableEdgeDrawer.jsx
@@ -4,21 +4,12 @@ import { Global } from "@emotion/react";
 import { styled } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { grey } from "@mui/material/colors";
-import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 const drawerBleeding = 56;
-
-const Root = styled("div")(({ theme }) => ({
-  height: "100%",
-  backgroundColor:
-    theme.palette.mode === "light"
-      ? grey[100]
-      : theme.palette.background.default,
-}));
 
 const StyledBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "light" ? "#fff" : grey[800],
@@ -47,7 +38,7 @@ function SwipeableEdgeDrawer(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Root>
+    <>
       <CssBaseline />
       <Global
         styles={{
@@ -97,7 +88,7 @@ function SwipeableEdgeDrawer(props) {
           <Skeleton variant="rectangular" height="100%" />
         </StyledBox>
       </SwipeableDrawer>
-    </Root>
+    </>
   );
 }
 
