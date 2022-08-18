2fccfc2f142ee333a48cb5ff4ef371159c17f854
dnwls7738 <cjsdnwls7738@gmail.com>
2022-08-18 16:07:44 +0900

    20220818

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Fx5000 from "./components/pages/Fx5000";
 import Hair from "./components/pages/Hair";
@@ -10,11 +10,13 @@ import PuraTech from "./components/pages/PuraTech";
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Main} />
-      <Route path="/ME" exact component={Me} />
-      <Route path="/FX-5000" exact component={Fx5000} />
-      <Route path="/HAIR" exact component={Hair} />
-      <Route path="/핵심기술" exact component={PuraTech} />
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/ME" exact component={Me} />
+        <Route path="/FX-5000" exact component={Fx5000} />
+        <Route path="/HAIR" exact component={Hair} />
+        <Route path="/핵심기술" exact component={PuraTech} />
+      </Switch>
     </Router>
   );
 }
