0c116da4cec008d76f084bb5fa47fbda5e6f2af1
dnwls7738 <cjsdnwls7738@gmail.com>
2022-09-15 12:02:15 +0900

    20220915

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { React, useEffect, useState } from "react";
-import { debounce } from "lodash";
+import { React } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
