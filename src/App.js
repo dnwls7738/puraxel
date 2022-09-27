a89d6078b968a124370fae9b796062aa719431f2
dnwls7738 <cjsdnwls7738@gmail.com>
2022-09-27 11:34:43 +0900

    20220927

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { React, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { MobileView, isMobile, isTablet } from "react-device-detect";
 
-import Fx5000 from "./components/pages/Fx5000";
-import Hair from "./components/pages/Hair";
+// import Fx5000 from "./components/pages/Fx5000";
+// import Hair from "./components/pages/Hair";
 // import Main from "./components/pages/Main";
-import Me from "./components/pages/Me";
-import PuraTech from "./components/pages/PuraTech";
+// import Me from "./components/pages/Me";
+// import PuraTech from "./components/pages/PuraTech";
 
 const REDIRECT_MOBILE = "https://dev.puraxel.co.kr/";
 
 const Main = lazy(() => import("./components/pages/Main"));
+const Fx5000 = lazy(() => import("./components/pages/Fx5000"));
+const Hair = lazy(() => import("./components/pages/Hair"));
+const Me = lazy(() => import("./components/pages/Me"));
+const PuraTech = lazy(() => import("./components/pages/PuraTech"));
 
 function App() {
   return isMobile || isTablet ? (
