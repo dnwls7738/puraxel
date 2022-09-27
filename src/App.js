5a3a82eae795d9f9dee79fa801fa49b7761e2be7
dnwls7738 <cjsdnwls7738@gmail.com>
2022-09-27 13:30:36 +0900

    20220927

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React, lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MobileView, isMobile, isTablet } from "react-device-detect";
 
 // import Fx5000 from "./components/pages/Fx5000";
@@ -21,17 +21,11 @@ function App() {
     <MobileView>
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/" exact component={Main} />
-
-            <Route path="/PURAXEL-Me" exact component={Me} />
-
-            <Route path="/PURAXEL" exact component={Fx5000} />
-
-            <Route path="/PURAXEL-Hair" exact component={Hair} />
-
-            <Route path="/핵심기술" exact component={PuraTech} />
-          </Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/PURAXEL-Me" exact component={Me} />
+          <Route path="/PURAXEL" exact component={Fx5000} />
+          <Route path="/PURAXEL-Hair" exact component={Hair} />
+          <Route path="/핵심기술" exact component={PuraTech} />
         </Suspense>
       </Router>
     </MobileView>
