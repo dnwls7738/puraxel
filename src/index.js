851bdbdf85878628d067c491e74a07a5ba515ef2
dnwls7738 <cjsdnwls7738@gmail.com>
2022-09-27 11:41:42 +0900

    20220927

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.scss";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 const rootNode = document.getElementById("root");
 
 ReactDOM.createRoot(rootNode).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>
 );
